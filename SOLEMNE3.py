fcacd9e642dc872b8ee147281f4c40b997a99eb0
L0OKASS <lucraft14738@gmail.com>
2024-11-26 00:18:33 -0300

    Update SOLEMNE3.py

--- SOLEMNE3.py
+++ SOLEMNE3.py
@@ -141,7 +141,7 @@ elif selected == "Contacto":
     
 with st.sidebar:
     ### Agregar el logo de la universidad ###
-    st.image("https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/Universidad_San_Sebasti%C3%A1n_%28logo%29.png/600px-Universidad_San_Sebasti%C3%A1n_%28logo%29.png", 
+    st.image("https://reqlut2.s3.sa-east-1.amazonaws.com/reqlut-images/uss/logo-original.png?v=65.6", 
              width=200)
     ### Menú principal ###
     selected = option_menu(
